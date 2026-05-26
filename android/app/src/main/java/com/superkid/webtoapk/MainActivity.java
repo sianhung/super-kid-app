fcc979820ffe82f04ddf1040685be31933e6678f
Richard <Richard@Richards-MacBook-Air.local>
2026-05-26 08:57:45 +0630

    feat: implement premium mobile portrait layout and dynamic video fullscreen landscape rotation

--- android/app/src/main/java/com/superkid/webtoapk/MainActivity.java
+++ android/app/src/main/java/com/superkid/webtoapk/MainActivity.java
@@ -702,6 +702,7 @@ public class MainActivity extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                     View.SYSTEM_UI_FLAG_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_IMMERSIVE);
+            setRequestedOrientation(android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
 
         @Override
